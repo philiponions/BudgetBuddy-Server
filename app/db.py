85cdd3335cb27289fec62ba177fe047378651bd3
slappy163 <105318087+slappy163@users.noreply.github.com>
2023-11-19 03:28:42 -0700

    gfsgrgesdf

--- app/db.py
+++ app/db.py
@@ -1,6 +1,8 @@
 import sqlite3
 
+
 def get_db_connection():
     conn = sqlite3.connect('data.db')
+    conn.execute(' PRAGMA foreign_keys=ON; ')
     conn.row_factory = sqlite3.Row
     return conn
