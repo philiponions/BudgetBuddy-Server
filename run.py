8487c86f0cc88c8a342bff4183bc50a352dfbc4f
Christian Eddie <122130586+Eddie1288@users.noreply.github.com>
2023-11-19 10:21:20 -0700

    Worked on run.py.

--- run.py
+++ run.py
@@ -1,4 +1,18 @@
 from app import create_app
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+ 
+@app.route("/")
+def showHomePage():
+    return "This is home page"
+ 
+@app.route("/debug", methods=["POST"])
+def debug():
+    text = request.form["sample"]
+    print(text)
+    return "received"
 
 if __name__ == '__main__':
     app = create_app()
